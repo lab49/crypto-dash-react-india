d115d8c0c1bac983fa9c565e8d12f5866591a62b
vinay <vinayprajapati107@gmail.com>
2021-10-21 17:39:30 +0530

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = ()=>{
+
+    return(
+        <footer className="footer">
+            <div className="container">
+                <span className="text-warning">&copy; Lab49. All Rights reserved</span>
+            </div>
+        </footer>
+    )
+}
+
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 const Header = () => {
     return (
-        <header>
-            <nav className="navbar navbar-dark bg-dark px-2">
-                <a className="navbar-brand" href="#">Crypto-trade</a>
-            </nav>
-        </header>
+        <div className="row mb-4">
+            <header>
+                <nav className="navbar navbar-dark bg-dark px-2">
+                    <a className="navbar-brand" href="#">Crypto-trade</a>
+                </nav>
+            </header>
+        </div>
     )
 }
 
