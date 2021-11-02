08a17ef18fadd9e2c10519fbdab4e9fd209c5d85
vinay <vinayprajapati107@gmail.com>
2021-11-02 11:32:33 +0530

    Generic dropdown component for Currency dropdown

--- src/components/common/CurrencyDropdown.js
+++ src/components/common/CurrencyDropdown.js
@@ -5,11 +5,11 @@ const CurrencyDropdown = ({ value: selectedValue, onChangeHandler, optionList, k
     const [isOpen, setIsOpen] = useState(false);
     const [optionsMap, setOptionsMap] = useState(new Map(optionList.map(option => [option.value, option])));
 
-    const toggleOpen = () => setIsOpen(!isOpen);
+    const toggleOpen = () => setIsOpen(prevState => !prevState);
 
     const getOption = (keyPrefix, { value, label, symbol }) => {
         return (
-            <li key={keyPrefix+value}
+            <li key={keyPrefix + value}
                 className={`dropdown-item ${selectedValue == value ? 'active' : ''}`}
                 onClick={(event) => onChangeHandler(value)}>
 
@@ -20,18 +20,25 @@ const CurrencyDropdown = ({ value: selectedValue, onChangeHandler, optionList, k
 
     return (
         <div className="dropdown w-100" onClick={toggleOpen}>
-            <button className="d-flex justify-content-between align-items-center w-100 btn btn-lg btn-secondary dropdown-toggle"
-                    type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            <button
+                className="d-flex justify-content-between align-items-center w-100 btn btn-lg btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
                 {
                     optionsMap ?
-                        <CurrencyRow name={optionsMap.get(selectedValue).label} symbol={optionsMap.get(selectedValue).symbol}/>
-                    : selectedValue
+                        <CurrencyRow name={optionsMap.get(selectedValue).label}
+                                     symbol={optionsMap.get(selectedValue).symbol}/>
+                        : selectedValue
                 }
             </button>
             <ul className={`w-100 dropdown-menu dropdown-menu-dark ${isOpen ? " show" : ""}`}
-                aria-labelledby="dropdownMenuButton1">
+                aria-labelledby="dropdownMenuButton1"
+            >
                 {
-                    optionList.map(option=> getOption(keyPrefix, option))
+                    optionList.map(option => getOption(keyPrefix, option))
                 }
             </ul>
         </div>
