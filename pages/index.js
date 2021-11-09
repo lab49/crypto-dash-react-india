b4409ff4c0ba1b1da4eb01dae811c0566e64f781
Amandeep Singh <amandeep.rehal@iongroup.com>
2021-11-09 17:29:07 +0530

    Live chart component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import MarketPageLayout from "../src/components/common/layouts/MarketPageLayout";
+import CurrencyHistoryChart from "../src/components/liveChart";
 
 const CryptoExchange = () => {
     return (
         <MarketPageLayout
             topMarketMoversComponent={''}
-            liveChartComponent={''}
+            liveChartComponent={<CurrencyHistoryChart
+                isAreaType={true}
+            />}
             quickTradeComponent={''}
             orderHistoryComponent={''}
         />
